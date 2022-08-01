1eb682acad9e8cb8658fffb51fe51db99a6df5c0
Ishwar Korade <99424917+ishwar-k@users.noreply.github.com>
2022-08-01 19:04:57 +0530

    Uploaded updated scripts

---
+++ datainsights.R
@@ -0,0 +1,22 @@
+install.packages("viridis")
+install.packages("ggpubr")
+library("viridis")
+library("ggpubr")
+library("tidyverse")
+library("data.table")
+
+##how to annual/casual members differ
+##why would casuals buy annual memberships
+##cyclistic use digimedia to influence casuals
+
+
+## Analysing the Dataset
+unique(combined_data$rideable_type)
+
+
+
+
+
+
+
+
